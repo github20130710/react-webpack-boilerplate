87b7bd960ca0e8468b4306b16b85f9715509f55c
sophia.wang <wangpp@chinaskycloud.com>
2018-05-03 19:13:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 这是一个 react＋webpack＋babel＋eslint 的脚手架。
+
+npm install
+
+npm start
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    contentBase: './app',
+    port: 8090
+  },
+  entry: [
+    'webpack-dev-server/client?http://localhost:8090',
+    path.resolve(__dirname, 'app/main.jsx')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: './bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader' },
+      { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new OpenBrowserPlugin({ url: 'http://localhost:8090' })
+  ]
+};
